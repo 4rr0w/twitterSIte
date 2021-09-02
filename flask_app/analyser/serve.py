db541ed33690efa14c88c81fd460467f0e412f5e
4rr0w <4rr0w.v@gmail.com>
2021-09-02 15:23:38 +0530

    fix wordcloud

--- flask_app/analyser/serve.py
+++ flask_app/analyser/serve.py
@@ -1,32 +1,48 @@
 import pandas as pd
 
+
 class Serve:
     def __init__(self, username, since, end) -> None:
         since_day = pd.to_datetime(since)
         end_day = pd.to_datetime(end)
-        self.df = pd.read_excel("./Sentimental/%s-tweets-analysed.xlsx" % username, engine= 'openpyxl')
-        self.df_filtered = self.df[(self.df['created_at'] >= since_day) & (self.df['created_at'] <= end_day)]
-        self.df_filtered_2020 = self.df[(self.df['created_at'] >= since_day) & (self.df['created_at'] <= end_day)]
+        self.df = pd.read_excel(
+            "./Sentimental/%s-tweets-analysed.xlsx" % username, engine='openpyxl')
+        self.df_filtered = self.df[(self.df['created_at'] >= since_day) & (
+            self.df['created_at'] <= end_day)]
+        self.df_filtered_2020 = self.df[(self.df['created_at'] >= since_day - pd.DateOffset(years=1)) & (
+            self.df['created_at'] <= end_day - pd.DateOffset(years=1))]
         self.username = username
 
-    def get_sentiments(self)-> dict:
+    def get_sentiments(self) -> dict:
         dict = {}
         dict["neutral"] = self.df_filtered[self.df_filtered["polarity"] == 0].shape[0]
-        dict["weak_positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0) & (self.df_filtered["polarity"] <= 0.3)].shape[0]
-        dict["positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0.3) & (self.df_filtered["polarity"] <= 0.6)].shape[0]
-        dict["strong_positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0.6) & (self.df_filtered["polarity"] <= 1)].shape[0]
-        dict["weak_negative"] = self.df_filtered[(self.df_filtered["polarity"] < 0) & (self.df_filtered["polarity"] >= -0.3)].shape[0]
-        dict["negative"] = self.df_filtered[(self.df_filtered["polarity"] < -0.3) & (self.df_filtered["polarity"] >= -0.6)].shape[0]
-        dict["strong_negative"] = self.df_filtered[(self.df_filtered["polarity"] < -0.6) & (self.df_filtered["polarity"] >= -1)].shape[0]
+        dict["weak_positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0) & (
+            self.df_filtered["polarity"] <= 0.3)].shape[0]
+        dict["positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0.3) & (
+            self.df_filtered["polarity"] <= 0.6)].shape[0]
+        dict["strong_positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0.6) & (
+            self.df_filtered["polarity"] <= 1)].shape[0]
+        dict["weak_negative"] = self.df_filtered[(self.df_filtered["polarity"] < 0) & (
+            self.df_filtered["polarity"] >= -0.3)].shape[0]
+        dict["negative"] = self.df_filtered[(
+            self.df_filtered["polarity"] < -0.3) & (self.df_filtered["polarity"] >= -0.6)].shape[0]
+        dict["strong_negative"] = self.df_filtered[(
+            self.df_filtered["polarity"] < -0.6) & (self.df_filtered["polarity"] >= -1)].shape[0]
 
         dict_2020 = {}
-        dict_2020["neutral"] = self.df_filtered[self.df_filtered["polarity"] == 0].shape[0]
-        dict_2020["weak_positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0) & (self.df_filtered["polarity"] <= 0.3)].shape[0]
-        dict_2020["positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0.3) & (self.df_filtered["polarity"] <= 0.6)].shape[0]
-        dict_2020["strong_positive"] = self.df_filtered[(self.df_filtered["polarity"] > 0.6) & (self.df_filtered["polarity"] <= 1)].shape[0]
-        dict_2020["weak_negative"] = self.df_filtered[(self.df_filtered["polarity"] < 0) & (self.df_filtered["polarity"] >= -0.3)].shape[0]
-        dict_2020["negative"] = self.df_filtered[(self.df_filtered["polarity"] < -0.3) & (self.df_filtered["polarity"] >= -0.6)].shape[0]
-        dict_2020["strong_negative"] = self.df_filtered[(self.df_filtered["polarity"] < -0.6) & (self.df_filtered["polarity"] >= -1)].shape[0]
+        dict_2020["neutral"] = self.df_filtered_2020[self.df_filtered_2020["polarity"] == 0].shape[0]
+        dict_2020["weak_positive"] = self.df_filtered_2020[(self.df_filtered_2020["polarity"] > 0) & (
+            self.df_filtered_2020["polarity"] <= 0.3)].shape[0]
+        dict_2020["positive"] = self.df_filtered_2020[(self.df_filtered_2020["polarity"] > 0.3) & (
+            self.df_filtered_2020["polarity"] <= 0.6)].shape[0]
+        dict_2020["strong_positive"] = self.df_filtered_2020[(
+            self.df_filtered_2020["polarity"] > 0.6) & (self.df_filtered_2020["polarity"] <= 1)].shape[0]
+        dict_2020["weak_negative"] = self.df_filtered_2020[(self.df_filtered_2020["polarity"] < 0) & (
+            self.df_filtered_2020["polarity"] >= -0.3)].shape[0]
+        dict_2020["negative"] = self.df_filtered_2020[(
+            self.df_filtered_2020["polarity"] < -0.3) & (self.df_filtered_2020["polarity"] >= -0.6)].shape[0]
+        dict_2020["strong_negative"] = self.df_filtered_2020[(
+            self.df_filtered_2020["polarity"] < -0.6) & (self.df_filtered_2020["polarity"] >= -1)].shape[0]
 
         dict_result = {
             "2020": dict_2020,
--- flask_app/analyser/wordcloud.py
+++ flask_app/analyser/wordcloud.py
@@ -9,21 +9,29 @@ class Wordcloud:
     def __init__(self, username, since, end) -> None:
         since_day = pd.to_datetime(since)
         end_day = pd.to_datetime(end)
-        self.df = pd.read_excel("./Sentimental/%s-tweets-analysed.xlsx" % username, engine= 'openpyxl')
+        self.df = pd.read_excel(
+            "./Sentimental/%s-tweets-analysed.xlsx" % username, engine='openpyxl')
         self.df_filtered = self.df[(self.df['created_at'] >= since_day) & (self.df['created_at'] <= end_day)]
-        self.df_filtered_2020 = self.df[(self.df['created_at'] >= since_day) & (self.df['created_at'] <= end_day)]
+        self.df_filtered_2020 = self.df[(self.df['created_at'] >= (since_day - pd.DateOffset(years=1))) & (
+            self.df['created_at'] <= (end_day - pd.DateOffset(years=1)))]
         self.username = username
 
-    def get_wordcloud(self)-> dict:
-        counts = pd.Series(np.concatenate([str(x).split() for x in self.df_filtered['no-stopword-text']])).value_counts()
-        sliced = counts.iloc[:200]
-
-        counts_2020 = pd.Series(np.concatenate([str(x).split() for x in self.df_filtered_2020['no-stopword-text']])).value_counts()
-        sliced_2020 = counts_2020.iloc[:200]
-        
+    def get_wordcloud(self) -> dict:
         dict = {
-            "2021": sliced.to_dict(),
-            "2020": sliced_2020.to_dict()
+            "2020": {},
+            "2021": {}
         }
+        if(not self.df_filtered['no-stopword-text'].empty):
+            counts = pd.Series(np.concatenate(
+                [str(x).split() for x in self.df_filtered['no-stopword-text']])).value_counts()
+            sliced = counts.iloc[:200]
+            dict["2021"] = sliced.to_dict()
+
+
+        if(not self.df_filtered_2020['no-stopword-text'].empty):
+            counts_2020 = pd.Series(np.concatenate(
+                [str(x).split() for x in self.df_filtered_2020['no-stopword-text']])).value_counts()
+            sliced_2020 = counts_2020.iloc[:200]
+            dict["2020"] = sliced_2020.to_dict()
+            
         return dict
-       
